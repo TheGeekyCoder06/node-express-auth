63b62a9253c9b1e7bd24f06da088b2dbe88917e5
Harshith M <mharshith200@gmail.com>
2025-10-22 08:11:04 +0530

    feat: Add Image model schema

---
+++ models/Image.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const ImageSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    publicId: {
+        type: String,
+        required: true,
+    },
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const Image = mongoose.model("Image", ImageSchema);
+
+export default Image;
